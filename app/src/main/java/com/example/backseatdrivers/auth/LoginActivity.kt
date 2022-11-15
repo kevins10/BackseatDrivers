d2d0fe31039fdd8de93174bbc64d5561fea3f67d
ChrisFong604 <ccffoonngg@gmail.com>
2022-11-15 02:37:30 -0800

    added sign-in logic; need to implement home activity to redirect to

--- app/src/main/java/com/example/backseatdrivers/auth/LoginActivity.kt
+++ app/src/main/java/com/example/backseatdrivers/auth/LoginActivity.kt
@@ -1,25 +1,80 @@
 package com.example.backseatdrivers.auth
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.backseatdrivers.database.User
 import com.example.backseatdrivers.databinding.ActivityLoginBinding
+import com.example.backseatdrivers.ui.home.dashboard.DashboardFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+
+    private lateinit var auth: FirebaseAuth
+    private var user: FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+
         binding.loginBtn.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val password = binding.passwordEt.text.toString()
+
+            if (email.isEmpty()) {
+                binding.emailEt.error = "Email invalid or not found"
+                return@setOnClickListener
+            }
+            if (password.isEmpty()) {
+                binding.passwordEt.error = "Password invalid"
+                return@setOnClickListener
+            }
+
+            login(email, password)
         }
 
         binding.signupBtn.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(user != null) {
+            val intent = Intent(this, VerifySignUpActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    user = auth.currentUser
+
+                    val intent = Intent(this, VerifySignUpActivity::class.java)
+                    intent.putExtra("user", user)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
         }
     }
 }
--- app/src/main/java/com/example/backseatdrivers/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/backseatdrivers/ui/profile/ProfileFragment.kt
@@ -26,7 +26,6 @@ class ProfileFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
