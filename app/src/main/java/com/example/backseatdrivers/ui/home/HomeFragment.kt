ed8d13feba67ae9db02fbf6b2be7cd09d6f1d6a2
Samantha <samttsee@gmail.com>
2022-12-03 15:54:29 -0800

    fix spacing and home fragment

--- app/src/main/java/com/example/backseatdrivers/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/backseatdrivers/ui/home/HomeFragment.kt
@@ -6,14 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.backseatdrivers.database.Ride
 import com.example.backseatdrivers.databinding.FragmentHomeBinding
+import com.example.backseatdrivers.ui.rides.RidesAdapter
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
-    private var upcomingRides = ArrayList<DataSnapshot>()
+    private lateinit var arrayList: ArrayList<Ride>
+    private lateinit var rideDatabase : DatabaseReference
+    private lateinit var userId: String
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,20 +32,69 @@ class HomeFragment : Fragment() {
     ): View {
         val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        homeViewModel.update()
-        homeViewModel.update.observe(viewLifecycleOwner) {
-            var homeAdapter = HomeAdapter(requireActivity(), upcomingRides)
-            var LV = binding.hfLv
-            LV.adapter = homeAdapter
-            upcomingRides = homeViewModel.getSnapshot()
-            homeAdapter.notifyDataSetChanged()
-            LV.adapter = homeAdapter
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null){
+            userId = user.uid
         }
 
-        return root
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //set on click listener for create a ride button
+        arrayList = arrayListOf()
+        val homeAdapter = RidesAdapter(requireActivity().applicationContext, arrayList)
+        val LV = binding.hfLv
+        LV.adapter = homeAdapter
+        if (LV != null) {
+            LV.setOnItemClickListener { parent, view, position, id ->
+
+                // open dialog with ride info
+
+            }
+        }
+
+        rideDatabase = Firebase.database.getReference("Rides")
+        rideDatabase.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                arrayList.clear()
+                for (i in snapshot.children){
+                    var ride : Ride = Ride()
+                    ride.ride_id = i.key
+                    ride.departure_time = i.child("departure_time").value.toString()
+                    ride.host_id = i.child("host_id").value.toString()
+                    ride.end_location = i.child("end_location").value.toString()
+                    ride.start_location = i.child("start_location").value.toString()
+                    ride.end_address = i.child("end_address").value.toString()
+                    ride.start_address = i.child("start_address").value.toString()
+                    ride.num_seats = i.child("num_seats").value.toString().toInt()
+                    val passengers = i.child("passengers").value
+
+                    // check if ride is posted by current user
+                    if (passengers != null) {
+                        ride.passengers = passengers as ArrayList<String>
+                        if (ride.passengers!!.size == ride.num_seats) {
+                            ride.is_full = true
+                        }
+                    }
+
+                    // add ride to array if hosted by user
+                    if (ride.host_id == userId) {
+                        arrayList.add(ride)
+                    }
+                }
+                LV.adapter = homeAdapter
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                //TODO("Not yet implemented")
+            }
+
+        })
     }
 
     override fun onDestroyView() {
