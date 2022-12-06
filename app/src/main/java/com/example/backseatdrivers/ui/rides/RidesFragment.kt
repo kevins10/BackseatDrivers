09eeb9c25d15f01252a35d26493de3be96c3f111
Kevin Shi <kevins.shi2001@gmail.com>
2022-12-06 01:38:53 -0800

    added filtering and limit to 3 destinations

--- app/src/main/java/com/example/backseatdrivers/ui/rides/RidesFragment.kt
+++ app/src/main/java/com/example/backseatdrivers/ui/rides/RidesFragment.kt
@@ -3,13 +3,16 @@ package com.example.backseatdrivers.ui.rides
 import android.app.Notification
 import android.content.ContentValues
 import android.content.Intent
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.Button
 import android.widget.ListView
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.backseatdrivers.R
@@ -25,6 +28,10 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.util.Collections.reverse
 import kotlin.collections.ArrayList
 
 class RidesFragment : Fragment() {
@@ -38,11 +45,13 @@ class RidesFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var arrayList: ArrayList<Ride>
     private lateinit var database: DatabaseReference
-    private lateinit var rideDatabase : DatabaseReference
+//    private lateinit var rideDatabase : DatabaseReference
     private lateinit var userViewModel: UserViewModel
-
+    var rideDatabase = Firebase.database.getReference("Rides").orderByChild("departure_time_milli")
     private lateinit var userId: String
-
+    private lateinit var filterSpinner: Spinner
+    private lateinit var LV : ListView
+    private lateinit var ridesAdapter : RidesAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -67,9 +76,12 @@ class RidesFragment : Fragment() {
         //createNotificationListener()
 
         //set on click listener for create a ride button
+        filterSpinner = view.findViewById(R.id.rides_filter)
+
+
         arrayList = arrayListOf()
-        var ridesAdapter = RidesAdapter(requireActivity().applicationContext, arrayList)
-        var LV = view.findViewById<ListView>(R.id.rides_lv)
+        ridesAdapter = RidesAdapter(requireActivity().applicationContext, arrayList)
+        LV = view.findViewById<ListView>(R.id.rides_lv)
         LV.adapter = ridesAdapter
         if (LV != null) {
             LV.setOnItemClickListener { parent, view, position, id ->
@@ -80,49 +92,70 @@ class RidesFragment : Fragment() {
 
             }
         }
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
+        val formatted = current.format(formatter).toString()
+        var longTime: Double = System.currentTimeMillis().toDouble()
+        update()
 
-        rideDatabase = Firebase.database.getReference("Rides")
-        rideDatabase.addValueEventListener(object : ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                arrayList.clear()
-                for (i in snapshot.children){
-                    var ride : Ride = Ride()
-                    ride.ride_id = i.key
-                    ride.departure_time = i.child("departure_time").value.toString()
-                    ride.host_id = i.child("host_id").value.toString()
-                    ride.end_location = i.child("end_location").value.toString()
-                    ride.start_location = i.child("start_location").value.toString()
-                    ride.end_address = i.child("end_address").value.toString()
-                    ride.start_address = i.child("start_address").value.toString()
-                    ride.num_seats = i.child("num_seats").value.toString().toInt()
-                    val passengers = i.child("passengers").value
-
-                    // check if ride is full
-                    if (passengers != null) {
-                        ride.passengers = passengers as HashMap<String, String>?
-                        if (ride.passengers!!.size == ride.num_seats) {
-                            ride.is_full = true
-                        }
-                    }
-
-                    // add ride to array if ride not full
-                    if (ride.is_full != true && ride.host_id != userId) {
-                        arrayList.add(ride)
-                    }
-                }
-                LV.adapter = ridesAdapter
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                //TODO("Not yet implemented")
-            }
-
-        })
         userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
         view.findViewById<Button>(R.id.createRideBtn).setOnClickListener {
             startCreateRideActivity()
         }
+
+        filterSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                if (parent != null) {
+                    if(parent.getItemAtPosition(position) as String != "None"){
+                        Firebase.database.reference.child("Rides").orderByChild("departure_time_milli").get()
+                            .addOnSuccessListener { it ->
+                                arrayList.clear()
+                                for (i in it.children) {
+                                    if (parent != null) {
+                                        if (i.child("end_address").value.toString().contains(parent.getItemAtPosition(position) as String)) {
+                                            var ride: Ride = Ride()
+                                            ride.ride_id = i.key
+                                            ride.departure_time = i.child("departure_time").value.toString()
+                                            ride.host_id = i.child("host_id").value.toString()
+                                            ride.end_location = i.child("end_location").value.toString()
+                                            ride.start_location = i.child("start_location").value.toString()
+                                            ride.end_address = i.child("end_address").value.toString()
+                                            ride.start_address = i.child("start_address").value.toString()
+                                            ride.num_seats = i.child("num_seats").value.toString().toInt()
+                                            val passengers = i.child("passengers").value
+                                            // check if ride is full
+                                            if (passengers != null) {
+                                                ride.passengers = passengers as HashMap<String, String>?
+                                                if (ride.passengers!!.size == ride.num_seats) {
+                                                    ride.is_full = true
+                                                }
+                                            }
+
+                                            // add ride to array if ride not full
+                                            if (ride.is_full != true && ride.host_id != userId) {
+                                                arrayList.add(ride)
+                                            }
+                                        }
+
+                                    }
+                                }
+                                LV.adapter = ridesAdapter
+                            }
+                    }
+
+                    else if(parent.getItemAtPosition(position) as String == "None"){
+                        update()
+                    }
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                //TODO("Not yet implemented")
+            }
+
+        }
     }
+
 
 //    private fun createNotificationListener() {
 //        val notificationListener = object : ValueEventListener {
@@ -161,5 +194,49 @@ class RidesFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun update(){
+        rideDatabase.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                arrayList.clear()
+                for (i in snapshot.children) {
+                    var ride: Ride = Ride()
+                    ride.ride_id = i.key
+                    ride.departure_time = i.child("departure_time").value.toString()
+                    ride.host_id = i.child("host_id").value.toString()
+                    ride.end_location = i.child("end_location").value.toString()
+                    ride.start_location = i.child("start_location").value.toString()
+                    ride.end_address = i.child("end_address").value.toString()
+                    ride.start_address = i.child("start_address").value.toString()
+                    ride.num_seats = i.child("num_seats").value.toString().toInt()
+                    ride.departure_time_milli =
+                        i.child("departure_time_milli").value.toString().toLong()
+                    val passengers = i.child("passengers").value
+
+                    val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy")
+                    val dateString = simpleDateFormat.format(ride.departure_time_milli)
+                    println("DATE BEING SAVED: ${dateString}")
+                    // check if ride is full
+                    if (passengers != null) {
+                        ride.passengers = passengers as HashMap<String, String>?
+                        if (ride.passengers!!.size == ride.num_seats) {
+                            ride.is_full = true
+                        }
+                    }
+
+                    // add ride to array if ride not full
+                    if (ride.is_full != true && ride.host_id != userId) {
+                        arrayList.add(ride)
+                    }
+                }
+                LV.adapter = ridesAdapter
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                //TODO("Not yet implemented")
+            }
+
+        })
     }
 }
